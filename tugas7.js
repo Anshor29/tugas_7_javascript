9a9559e1e0068a5857b3e44a523c30754cd935f9
Anshor <anshorjihadi@gmail.com>
2019-09-26 18:26:04 +0700

    Tugas 7 Js

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+var satu = [6,7,5,8,4,9]
+console.log("For Biasa");
+
+for (i=0; i<satu.length;i++){
+  console.log(satu[i]);
+}
+
+var dua = [9,8,7,6,5,4]
+console.log("For Of");
+
+for (let x of dua){
+  console.log(x);
+}
